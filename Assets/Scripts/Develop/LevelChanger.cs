123df7cb7e86dfbd2685b9832e51394e2f35dd0d
N_S <evilnood666@gmail.com>
2024-03-13 23:33:53 +0200

    added enabling collider for doors

--- Assets/Scripts/Develop/LevelChanger.cs
+++ Assets/Scripts/Develop/LevelChanger.cs
@@ -16,6 +16,7 @@ public class LevelChanger : MonoBehaviour
     void OpenChanger()
     {
         canLoadLevel = true;
+        GetComponent<Collider>().enabled = true;
     }
     private void OnTriggerEnter(Collider other)
     {
